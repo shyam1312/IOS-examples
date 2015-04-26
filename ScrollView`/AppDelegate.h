98d2dfc14235bc91c13eee8028141b74addd0e97
shyam <shyam@shyams-Mac.local>
2015-04-26 06:54:48 -0700

    Initial Commit

---
+++ ScrollView`/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView`
+//
+//  Created by shyam on 4/26/15.
+//  Copyright (c) 2015 shyam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView`/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView`
+//
+//  Created by shyam on 4/26/15.
+//  Copyright (c) 2015 shyam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
